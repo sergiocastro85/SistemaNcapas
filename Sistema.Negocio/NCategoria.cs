00bc6e60e1dd7c24acd17448212078b99946a188
sergiocastro85 <sergiocastro86@gmail.com>
2020-09-09 07:56:36 -0500

    Modificaciones en capa de negocio

--- Sistema.Negocio/NCategoria.cs
+++ Sistema.Negocio/NCategoria.cs
@@ -14,7 +14,54 @@ namespace Sistema.Negocio
     {
         public static DataTable Listar()
         {
+            DCategoria Datos = new DCategoria();
+            return Datos.listar();
 
         }
+
+        public static DataTable Buscar(string Valor)
+        {
+            DCategoria Datos= new DCategoria();
+            return Datos.Buscar(Valor);
+        }
+        public static string Insertar(string Nombre,string Descripcion)
+        {
+            DCategoria Datos = new DCategoria();
+            Categoria obj = new Categoria();
+            obj.Nombre = Nombre;
+            obj.Descripcion = Descripcion;
+            return Datos.Insertar(obj);
+        }
+        public static string Actualizar (int Id, string Nombre, string Descripcion)
+        {
+            DCategoria Datos = new DCategoria();
+            Categoria obj = new Categoria();
+            obj.IdCategoria = Id;
+            obj.Nombre = Nombre;
+            obj.Descripcion = Descripcion;
+            return Datos.Actualizar(obj);
+                
+        }
+        public static string Eliminar(int Id)
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Eliminar(Id);
+            
+        }
+        public static string Activar(int Id)
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Activar(Id);
+        }
+        public static string Desactivar(int Id)
+        {
+            DCategoria Datos = new DCategoria();
+            return Datos.Desactivar(Id)
+
+        }
+
+
+
+
     }
 }

21765bb50b89928475da7d682bf6cdff9a88dac8
sergiocastro85 <sergiocastro86@gmail.com>
2021-01-07 08:26:14 -0500

    Capa Datos : usuario

--- Sistema.Entidades/Usuario.cs
+++ Sistema.Entidades/Usuario.cs
@@ -12,11 +12,15 @@ namespace Sistema.Entidades
         
         public int IdRol { get; set; }
 
+        public string TipoDocumento { get; set; }
+
         public string Nombre { get; set; }
 
         public string  NumDocumento { get; set; }
 
         public string Direccion { get; set; }
+
+        public string Telefono { get; set; }
 
         public string Email { get; set; }
 
